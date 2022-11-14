3a8d1db2a6e6b060ba3866597132101552295cb2
Youssef Fray <yousseffray001@gmail.com>
2022-11-15 00:19:22 +0100

    client side

--- ChatClient.java
+++ ChatClient.java
@@ -1,48 +1,47 @@
 import java.awt.EventQueue;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+ import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
-import java.awt.event.ActionListener;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.awt.event.ActionEvent;
 import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ChatServer {
+public class ChatClient {
 
 	private JFrame frame;
 	private JTextField textField;
-	private static JTextArea textArea1  ;
-	private JButton btnNewButton ;
-	static ServerSocket server ;
+	public static JTextArea textArea ;
 	static Socket con ;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ChatServer window = new ChatServer();
+					ChatClient window = new ChatClient();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		 server= new ServerSocket(5032);
-		 con=server.accept();	
+		 
+		con = new Socket("127.0.0.1",5032);
 		 while(true)
 		 {
 		 try {
 			DataInputStream input = new DataInputStream(con.getInputStream());
 			 String string = input.readUTF();
-			 textArea1.setText(textArea1.getText()+"\n "+"Client: "+string);
+			 System.out.println(string);
+ 			 textArea.setText(textArea.getText()+"\n "+"server : "+string);
 		} catch (IOException e) {
-			 textArea1.setText(textArea1.getText()+"\n "+"error");
+			 textArea.setText(textArea.getText()+"\n "+"error");
 			 try {
 				 Thread.sleep(2000);
 				System.exit(0);
@@ -55,15 +54,14 @@ public class ChatServer {
 	}
 
 
-	public ChatServer() throws IOException {
-		initialize();
 
+	public ChatClient() {
+		initialize();
 	}
 
-
-	private void initialize() throws IOException {
+	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(UIManager.getColor("Button.disabledForeground"));
+		frame.getContentPane().setBackground(UIManager.getColor("Button.darkShadow"));
 		frame.setBounds(100, 100, 526, 357);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -73,14 +71,15 @@ public class ChatServer {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		 btnNewButton = new JButton("send");
+		JButton btnNewButton = new JButton("send");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+		 
 				try {
 					DataOutputStream output = new DataOutputStream(con.getOutputStream());
 					output.writeUTF(textField.getText());
 				} catch (IOException e1) {
-					textArea1.setText(textArea1.getText()+"\n "+ "error");
+					textArea.setText(textArea.getText()+"\n "+ "error");
  					try {
 						Thread.sleep(2000);
 						System.exit(0);
@@ -89,13 +88,13 @@ public class ChatServer {
 					}
 				}
 				textField.setText("");
+				
 			}
 		});
 		btnNewButton.setBounds(354, 45, 110, 39);
 		frame.getContentPane().add(btnNewButton);
-		
-	    textArea1 = new JTextArea();
-		textArea1.setBounds(20, 134, 291, 161);
-		frame.getContentPane().add(textArea1);
+		textArea = new JTextArea();
+		textArea.setBounds(20, 134, 291, 161);
+		frame.getContentPane().add(textArea);
 	}
 }
